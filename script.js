3ac711ec91a0f8af1460f8e2b619e7778e3ae709
JAOscoy <ja.oscoyml@gmail.com>
2020-09-21 01:40:20 -0500

    todo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-if (47 < 50) {
- showMessage("que paso");
+function comunicado(valor) {
+    document.getElementById('Mensajito').textContent = valor;
+}
+
+let pr = 0;
+
+if (pr>44) {
+    comunicado("Si");
+}
+else if (pr<=44 && pr>1) {
+    comunicado("No");
+}
+else if (pr == 0) {
+    comunicado("Oh no");
 }
