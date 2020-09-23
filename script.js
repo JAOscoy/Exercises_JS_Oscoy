387150b1bdf5ac18928c2cd80095db3582d90e83
JAOscoy <ja.oscoyml@gmail.com>
2020-09-22 23:57:34 -0500

    a

--- script.js
+++ script.js
@@ -22,4 +22,45 @@ const Oscoy = {
         color_2: "verde"
     }
 }
+/*
+  Clase
 
+  Es una plantilla que define a un objeto.
+
+  Se recomienda que el nombre de la clase
+  sea con "PascalCase"
+*/
+class Carro {
+    /*
+      El constructor es un método/función especial
+      que es ejecutado una única vez
+      al utilizar el operador "new".
+  
+      Por lo general en JS, se utiliza
+      para definir los atributos de la clase.
+    */
+    constructor(velocidad, marca, modelo) {
+      console.log("Ejecutando constructor...");
+      this.velocidad = velocidad;
+      this.marca = marca;
+      this.modelo = modelo;
+    }
+  }
+  
+  /*
+    Para crear un objeto a partir de una clase
+    se utiliza el operador "new".
+  
+    A los objetos creados con el operador new se les llama
+    "instancias" de la clase.
+  */
+  const bmw = new Carro();
+  const volkswagen = new Carro();
+  let chevrolet = new Carro();
+  const chrysler = new Carro (67, "Trololo", "Tsubami");
+
+  
+  console.log('bmw', bmw);
+  console.log('vo')
+
+  
